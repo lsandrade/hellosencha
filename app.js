859ad1014969771c4ed0d96582336ccf5460b88f
Luan Andrade <luansandrade92@hotmail.com>
2015-05-12 23:05:26 -0300

    Herança EXT/Sencha
    Uso de herança com EXT/Sencha atraves do componente Ext.Button

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
+
 Ext.application({
-	name: 'Sencha',
+			name: 'Sencha',
 
-	launch: function(){
-		//alert('launched');
-		Ext.create('Ext.Panel',{
-			fullscreen: true,
-			html: 'Hello World'
+			launch: function(){
+
+				var painel = Ext.create('Ext.Panel',{
+					fullscreen: true,
+					html: 'Hello World'
+				});
+				//instanciando um objeto User
+				var usuario = Ext.create('User',{ //cria a classe user
+					nome: 'Luan', //construtor
+					senha: '12345'
+				});
+				usuario.login();
+				usuario.logout();
+				console.log(usuario); //mostra o objeto usuario
+
+				var botao= Ext.create('MeuBotao',{
+						text: 'Meu Botao',
+						renderTo: 'botao01'
+					});
+				 //Ext.Viewport.add(botao);
+			}
+
 		});
-	}
-
-});
---
+++ meubotao.js
@@ -0,0 +1,7 @@
+Ext.define('MeuBotao',{
+	extend: 'Ext.Button',
+
+	initComponent: function(){
+
+	}		
+});
