f922263dbed377f3b2d8f512eff67979646239cd
Luan Andrade <luansandrade92@hotmail.com>
2015-05-14 20:07:30 -0300

    Model Validations

--- mvc/app.js
+++ mvc/app.js
@@ -7,7 +7,7 @@ Ext.application({
 			name: 'Sencha',
 
 			launch: function(){
-
+				//Criação de um funcionário do Model
 				var funcionario = Ext.create('model.Funcionario',{
 					nome: 'Luan',
 					idade: 22,
@@ -17,9 +17,19 @@ Ext.application({
 					usuario: 'luan'
 				});
 				
-				funcionario.set('nome','Pablo');
+				funcionario.set('nome','Pablo'); //setando nome
 				console.log(funcionario.get('nome'));
-				
+
+				var funcionario2= Ext.create('model.Funcionario',{
+					nome:'Luan',
+					genero: 'M',
+					usuario: 'luan',
+					idade: 25
+				});
+				var errors = funcionario2.validate();
+				console.log(errors.isValid());
+				console.log(errors.items);
+				console.log(errors.getByField('nome'));
 			}
 
 		});
