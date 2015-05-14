018be34929ba81e3bda5c44d1a6cf5fce191792c
Luan Andrade <luansandrade92@hotmail.com>
2015-05-14 20:04:53 -0300

    Model Validations no Sencha/EXT
    Só funciona com config

--- mvc/model/Funcionario.js
+++ mvc/model/Funcionario.js
@@ -1,5 +1,6 @@
 Ext.define('model.Funcionario',{
 	extend: 'Ext.data.Model',
+	config:{
 	fields: [
 		{name: 'nome'}, //tipo: auto
 		{name: 'idade', type: 'int'}, //tipo: int
@@ -11,6 +12,17 @@ Ext.define('model.Funcionario',{
 			convert: function(value,record){
 				return record.get('usuario') + '@email.com';
 			}
-		}
-	]
+		},
+		{name:'genero',type:'string'}
+	],
+	validations: [
+			{type: 'presence', field: 'nome'},
+			{type: 'presence', field: 'usuario'},
+			{type: 'presence', field: 'idade'},
+			{type: 'length', field: 'usuario', min: 3, max: 10},
+			{type: 'format', field: 'nome', matcher: /([a-z]+)/},
+			{type: 'inclusion', field: 'genero', list:['F','M']},
+			{type: 'exclusion', field: 'idade', list:[0]},
+		]
+	}
 });
